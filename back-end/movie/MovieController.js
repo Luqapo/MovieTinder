424e5812f3aad50026be38ab00e47b316252932f
Lucjan Wiśnicz <luqapo@gmail.com>
2018-12-09 18:33:38 +0100

    movieController

--- back-end/movie/MovieController.js
+++ back-end/movie/MovieController.js
@@ -9,5 +9,20 @@ router.use(bodyParser.json());
 
 router.route('/movie')
     .post((req, res) => {
-        
+        Movie.creater({
+            title: req.body.title,
+            imageURL: req.body.imageURL,
+            summary: req.body.summary,
+            rating: req.body.rating
+        }, (err, movie) => {
+            if (err) return res.status(500).send('There was a problem crating movie.')
+            res.status(200).send( 'Movie created' );
+        });
+    })
+    .get((req, res) => {
+        Movie.find((err, movies) => {
+            if(err) res.status(500).send('Error on the server');
+            if(!movies) res.status(404).send('No movies found.');
+            res.status(200).json(movies);
+        })
     })
